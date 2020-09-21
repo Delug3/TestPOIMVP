48333aeff99b18dc6fddc6c9b927f4394be7a9cb
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-09-21 11:54:41 +0200

    updated insertall method

--- app/src/main/java/com/delug3/testpoi/database/dao/PoiDao.kt
+++ app/src/main/java/com/delug3/testpoi/database/dao/PoiDao.kt
@@ -5,24 +5,21 @@ package com.delug3.testpoi.database.dao
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.delug3.testpoi.database.entity.PoiRoom
-import java.util.ArrayList
 
 @Dao
 interface PoiDao {
 
-    @Insert
-    fun insert(poiRoom: PoiRoom)
+   @Insert(onConflict = OnConflictStrategy.IGNORE)
+   fun insert(poiRoom: PoiRoom)
 
    @Insert(onConflict = REPLACE)
-   fun insertAllPois(poiRoom: ArrayList<PoiRoom>?);
-
+   fun insertAllPois(poisRoomList: List<PoiRoom?>?);
 
    @Query("SELECT * FROM poi_table")
    fun getAllPois(): LiveData<List<PoiRoom>>
-
-
 
 }
