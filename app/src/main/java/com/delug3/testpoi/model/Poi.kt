c3ba9c43cfb63b7bd4eb585ce3f6be9e4d019f71
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-10-13 11:38:15 +0200

    added constructor

--- app/src/main/java/com/delug3/testpoi/model/Poi.kt
+++ app/src/main/java/com/delug3/testpoi/model/Poi.kt
@@ -1,11 +1,22 @@
 package com.delug3.testpoi.model
 
 class Poi {
-    lateinit var id: String
+    var id: String
     var title: String? = null
     var address: String? = null
     var transport: String? = null
     var email: String? = null
     var geocoordinates: String? = null
     var description: String? = null
+
+
+    constructor(id: String, title: String?, address: String?, transport: String?, email: String?, geocoordinates: String?, description: String?) {
+        this.id = id
+        this.title = title
+        this.address = address
+        this.transport = transport
+        this.email = email
+        this.geocoordinates = geocoordinates
+        this.description = description
+    }
 }
