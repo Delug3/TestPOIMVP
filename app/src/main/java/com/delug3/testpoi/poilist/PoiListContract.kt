5345de52ce7de29ba999bc76e8e40b8d42fb5882
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-10-13 11:41:37 +0200

    updated parameters in multiple functions

--- app/src/main/java/com/delug3/testpoi/poilist/PoiListContract.kt
+++ app/src/main/java/com/delug3/testpoi/poilist/PoiListContract.kt
@@ -10,12 +10,12 @@ interface PoiListContract {
     interface Model {
         interface OnFinishedListener {
             fun onFinished(poiList: List<Poi?>?,mappedPoiRoom: List<PoiRoom?>?)
-            fun onDetailsFinished(title: String?, address: String?, transport: String?, email: String?, geocoordinates: String?, description: String?)
+            fun onDetailsFinished(idPoi: String, title: String?, address: String?, transport: String?, email: String?, geocoordinates: String?, description: String?)
             fun onFailure(t: Throwable?)
         }
 
         fun getOnlineData(onFinishedListener: OnFinishedListener?)
-        fun getPoiDetails(onFinishedListener: OnFinishedListener?, idPoi: String?)
+        fun getPoiDetails(onFinishedListener: OnFinishedListener?, idPoi: String)
         fun getOfflineData(onFinishedListener: OnFinishedListener?)
     }
 
@@ -24,6 +24,7 @@ interface PoiListContract {
         fun hideProgress()
         fun sendDataToRecyclerView(poiOnlineList: List<Poi?>?)
         fun sendDataToRoomDataBase(poiOfflineList: List<PoiRoom?>?)
+        fun updateFieldInRoomDataBase(idPoi: String, address: String?, transport: String?, email: String?, description: String?)
         fun showPoiDetails(title: String?, address: String?, transport: String?, email: String?, geocoordinates: String?, description: String?)
         fun onResponseFailure(throwable: Throwable?)
     }
@@ -31,7 +32,7 @@ interface PoiListContract {
     interface Presenter {
         fun onDestroy()
         fun requestAllDataFromUrl()
-        fun requestDataDetails(idPoi: String?)
+        fun requestDataDetails(idPoi: String)
         fun requestDataFromDataBase()
     }
 }
