9e53d8b220987219470bfdd2a1f01d4452ecc868
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-10-13 11:36:52 +0200

    added readAll and readSingle methods

--- app/src/main/java/com/delug3/testpoi/database/dao/PoiDao.kt
+++ app/src/main/java/com/delug3/testpoi/database/dao/PoiDao.kt
@@ -10,6 +10,7 @@ import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.delug3.testpoi.database.entity.PoiRoom
 
+
 @Dao
 interface PoiDao {
 
@@ -20,6 +21,18 @@ interface PoiDao {
    fun insertAllPois(poisRoomList: List<PoiRoom?>?);
 
    @Query("SELECT * FROM poi_table")
-   fun getAllPois(): LiveData<List<PoiRoom>>
+   fun readAllPois(): LiveData<List<PoiRoom>>
+
+   @Query("SELECT * FROM poi_table WHERE id=:id ")
+   fun readSinglePoi(id: String): LiveData<List<PoiRoom>>
+
+   /*@Update
+   fun updatePoi(poiRoom: PoiRoom)
+   */
+
+
+  @Query("UPDATE poi_table SET address = :address, transport = :transport, email = :email, description = :description WHERE id =:id")
+   fun updatePoi(id: String, address: String?, transport: String?, email: String?, description: String?)
+
 
 }
