8cac8d3f9b337ec057b82ea63d1be1a7c49a263b
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-11-11 12:30:44 +0100

    added viewbinding to adapter

--- app/src/main/java/com/delug3/testpoi/adapter/PoisAdapter.kt
+++ app/src/main/java/com/delug3/testpoi/adapter/PoisAdapter.kt
@@ -1,14 +1,12 @@
 package com.delug3.testpoi.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.delug3.testpoi.R
 import com.delug3.testpoi.database.entity.PoiRoom
+import com.delug3.testpoi.databinding.ItemPoisBinding
 import com.delug3.testpoi.model.Poi
 import com.delug3.testpoi.poilist.PoiListActivity
 import java.util.*
@@ -16,21 +14,21 @@ import java.util.*
 class PoisAdapter(private val poiListActivity: PoiListActivity, private val poiList: MutableList<Poi?>) : RecyclerView.Adapter<PoisAdapter.ViewHolder>(), Filterable {
 
     private var poiListFiltered: List<Poi?>
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_pois, parent, false)
-        return ViewHolder(view)
+        val binding = ItemPoisBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val poi = poiListFiltered[position]
-        holder.textViewTitle.text = poi?.title
-        holder.textViewGeocoordinates.text = poi?.geocoordinates
+        holder.bind(poi)
         holder.itemView.setOnClickListener { poiListActivity.onPoiItemClick(position) }
     }
 
     internal fun setPois(poisFromDataBase: List<PoiRoom>) {
 
-        val mappedRoomList = poisFromDataBase?.map { Poi(it.id, it.title, it.address, it.transport, it.email, it.geocoordinates, it.description) }
+        val mappedRoomList = poisFromDataBase.map { Poi(it.id, it.title, it.address, it.transport, it.email, it.geocoordinates, it.description) }
 
         this.poiListFiltered = mappedRoomList
         notifyDataSetChanged()
@@ -53,9 +51,9 @@ class PoisAdapter(private val poiListActivity: PoiListActivity, private val poiL
                     poiListFiltered = poiList
                 } else {
                     val filteredList: MutableList<Poi?> = ArrayList()
-                    val filterPattern = charSequence.toString().toLowerCase().trim { it <= ' ' }
+                    val filterPattern = charSequence.toString().toLowerCase(Locale.ROOT).trim { it <= ' ' }
                     for (poi in poiList) {
-                        if (poi?.title!!.toLowerCase().contains(filterPattern)) {
+                        if (poi?.title!!.toLowerCase(Locale.ROOT).contains(filterPattern)) {
                             filteredList.add(poi)
                         }
                     }
@@ -73,13 +71,10 @@ class PoisAdapter(private val poiListActivity: PoiListActivity, private val poiL
         }
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textViewTitle: TextView
-        val textViewGeocoordinates: TextView
-
-        init {
-            textViewTitle = itemView.findViewById(R.id.text_view_title)
-            textViewGeocoordinates = itemView.findViewById(R.id.text_view_geocoordinates)
+    class ViewHolder(val binding: ItemPoisBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(poi: Poi?) {
+            binding.textViewTitle.text = poi?.title
+            binding.textViewGeocoordinates.text = poi?.geocoordinates
         }
     }
 
