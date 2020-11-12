6b057e63fa27d963b33c9ee563f433c838a5291a
Manuel Fernandez Garcia <mfgarcia87@gmail.com>
2020-11-12 11:38:21 +0100

    cleaned code

--- app/src/main/java/com/delug3/testpoi/model/Poi.kt
+++ app/src/main/java/com/delug3/testpoi/model/Poi.kt
@@ -1,22 +1,11 @@
 package com.delug3.testpoi.model
 
-class Poi {
-    var id: String
-    var title: String? = null
-    var address: String? = null
-    var transport: String? = null
-    var email: String? = null
-    var geocoordinates: String? = null
-    var description: String? = null
+class Poi(var id: String,
+          var title: String?,
+          var address: String?,
+          var transport: String?,
+          var email: String?,
+          var geocoordinates: String?,
+          var description: String?)
 
 
-    constructor(id: String, title: String?, address: String?, transport: String?, email: String?, geocoordinates: String?, description: String?) {
-        this.id = id
-        this.title = title
-        this.address = address
-        this.transport = transport
-        this.email = email
-        this.geocoordinates = geocoordinates
-        this.description = description
-    }
-}
